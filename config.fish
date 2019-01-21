01218cff4b05169be2b430c51b978558d5585e1b
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-01-21 13:52:50 +0100

    fix fish

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 alias m="multipass"
 alias loc="git ls-files | xargs cat | wc -l"
-alias reload="source ~/.zshrc"
+## GIT
 alias gco="git checkout"
 alias gst="git status -sb"
 alias gc="git commit"
@@ -10,12 +10,17 @@ alias gaa="git add -A"
 alias gap="git add --patch"
 alias gpl="git pull"
 alias gps="git push"
+
+
 alias ugh="del node_modules/ && yarn install"
+
 alias ..="cd .."
 alias crontab="VIM_CRONTAB=true crontab"
 alias aws_creds="cat ~/.aws/credentials | grep '\[' | awk -F'[][]' '{print $2}'"
+
 alias map="xargs -n1"
+
 alias pubip="dig +short myip.opendns.com @resolver1.opendns.com"
 
-set --global EDITOR "nvim"
+set --global EDITOR "vim"
 set -gx PATH ~/.dotfiles/bin $PATH
