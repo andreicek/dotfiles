889791e2b1eb9bc7210bea4b871d8fffb01ed28f
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-09-18 10:50:45 +0200

    fake ssh for prompt

--- config.fish
+++ config.fish
@@ -1,6 +1,5 @@
 alias ls "exa --long --git"
 alias rm "trash"
-abbr title "it2setkeylabel set status"
 abbr map "xargs -n1"
 abbr gco "git checkout"
 abbr gst "git status -sb"
@@ -12,20 +11,33 @@ abbr gap "git add --patch"
 abbr gpl "git pull"
 abbr gps "git push"
 
-# My prompt
-function fish_prompt
-
+function touchbar_prompt
   if type -q it2setkeylabel
-    it2setkeylabel set status (prompt_pwd)
+    set -l pwd (prompt_pwd)
+    set -l git_branch (git branch ^/dev/null | grep \* | sed 's/* //')
+    if test "$git_branch" = ""
+      it2setkeylabel set status "$pwd"
+    else
+      it2setkeylabel set status "$pwd ($git_branch)"
+    end
   end
+end
 
+function fish_prompt
+  touchbar_prompt
   set_color $fish_color_cwd
   echo -n (basename $PWD)
   set_color normal
   echo -n ' ) '
 end
 
-# ENVs
 set -gx EDITOR "vim"
 test -e {$HOME}/.config/fish/secret.fish; and source {$HOME}/.config/fish/secret.fish
 set -Ua fish_user_paths {$HOME}/.dotfiles/bin
+
+if type -q it2setkeylabel
+  function ssh
+    it2setkeylabel set status "ssh $argv"
+    /usr/bin/ssh $argv
+  end
+end
