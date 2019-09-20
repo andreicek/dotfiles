20e3b9871c2296c30d18c4b50a8adeed26fdda2f
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-09-20 12:00:29 +0200

    fix logging

--- install.sh
+++ install.sh
@@ -5,12 +5,15 @@ echo "Setting up dotfiles"
 cd $HOME
 git clone https://github.com/andreicek/dotfiles .dotfiles
 
-echo "Fish shell"
+echo -n "Fish shell"
 mkdir -p $HOME/.config/fish
 ln -s $HOME/.dotfiles/config.fish $HOME/.config/fish/config.fish
+echo " done."
 
-echo "Git"
+echo -n "Git"
 ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+echo " done."
 
-echo "Vim"
+echo -n "Vim"
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+echo " done."
