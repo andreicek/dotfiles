7268fd2386b5e2df909e653cf903afc56e94fe92
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-09-23 16:38:49 +0200

    add vsc setup

--- install.sh
+++ install.sh
@@ -17,6 +17,14 @@ if grep -q "Darwin" <<< $(uname -a); then
 
   sudo mkdir -p /usr/local/n
   sudo chown -R $(whoami):staff /usr/local/n
+
+  code --install-extension dbaeumer.vscode-eslint
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension ms-azuretools.vscode-docker
+  code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+  code --install-extension sianglim.slim
+  code --install-extension skyapps.fish-vscode
+  code --install-extension tootone.org-mode
 fi
 
 if ! grep -q "$(which fish)" /etc/shells; then
@@ -35,6 +43,8 @@ ln -s $HOME/.dotfiles/config.fish $HOME/.config/fish/config.fish
 ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
 
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+
+ln -s $HOME/.dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 cd $HOME/.dotfiles
 git remote set-url origin git@github.com:andreicek/dotfiles.git
