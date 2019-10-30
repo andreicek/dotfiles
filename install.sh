7fe9a4fa370e76ee005ee2df26f9b5162c008fd2
Andrei Zvonimir Crnkovic <andrei@infinum.com>
2019-10-30 15:09:04 +0100

    Add starship to install

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ if grep -q "Darwin" <<< $(uname -a); then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
 
-  brew install exa git node postgresql rbenv bat fish httpie n ripgrep jq
+  brew install exa git node postgresql rbenv bat fish httpie n ripgrep jq starship
 
   brew cask install visual-studio-code google-chrome \
     firefox slack 1password docker iterm2 imageoptim \
-    resilio-sync spotify tableplus zoomus whatsapp starship
+    resilio-sync spotify tableplus zoomus whatsapp
 
   if ! grep -q "auth sufficient pam_tid.so" /etc/pam.d/sudo; then
     awk 'NR==2{print "auth sufficient pam_tid.so"}1' /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo
