44dfbad607047d38d834a38676d27a0e6f584e23
Andrei Zvonimir Crnkovic <andreicek@0x7f.dev>
2021-11-22 10:52:18 +0100

    Switch to fish, again

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+set -gx GPG_TTY (tty)
+
+if status is-interactive
+    starship init fish | source
+    set -gx EDITOR "vim"
+
+    alias gco="git checkout"
+    alias gst="git status -sb"
+    alias gc="git commit"
+    alias gcm="git commit -m"
+    alias ga="git add"
+    alias gaa="git add -A"
+    alias gap="git add --patch"
+    alias gpl="git pull"
+    alias gps="git push"
+end
