5a82d1ac2cb9482141be8ff716a78f8d56bc6d52
Andrei Zvonimir Crnkovic <andreicek@0x7f.dev>
2022-01-18 17:06:05 +0100

    Switch to sublime

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,18 @@
-set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
-set -U fish_user_paths ~/.emacs.d/bin $fish_user_paths
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 set -gx GPG_TTY (tty)
 set -gx EDITOR "vim"
 set -gx  ERL_AFLAGS "-kernel shell_history enabled"
-
-alias intel_brew="arch -x86_64 /usr/local/homebrew/bin/brew"
+source (brew --prefix asdf)/libexec/asdf.fish
 
 if status is-interactive
-    starship init fish | source
-
-    alias gco="git checkout"
-    alias gst="git status -sb"
-    alias gc="git commit"
-    alias gcm="git commit -m"
-    alias ga="git add"
-    alias gaa="git add -A"
-    alias gap="git add --patch"
-    alias gpl="git pull"
-    alias gps="git push"
-
-    alias cat="bat"
+  alias gco="git checkout"
+  alias gst="git status -sb"
+  alias gc="git commit"
+  alias gcm="git commit -m"
+  alias ga="git add"
+  alias gaa="git add -A"
+  alias gap="git add --patch"
+  alias gpl="git pull"
+  alias gps="git push"
+  alias cat="bat"
 end
-
-source (brew --prefix asdf)/libexec/asdf.fish
