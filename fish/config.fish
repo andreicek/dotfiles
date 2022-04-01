c908ec6ac0ed9f3bf483c18947880ffc146c9e72
Andrei Zvonimir Crnkovic <andreicek@0x7f.dev>
2022-04-01 22:02:38 +0200

    Add vscode

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,15 @@
-set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
-set -gx GPG_TTY (tty)
 set -gx EDITOR "nvim"
-set -gx  ERL_AFLAGS "-kernel shell_history enabled"
-source (brew --prefix asdf)/libexec/asdf.fish
+set -gx GPG_TTY (tty)
+set -gx ERL_AFLAGS "-kernel shell_history enabled"
+
+if test -d /opt/homebrew/bin
+  set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+  source (brew --prefix asdf)/libexec/asdf.fish
+end
 
 if status is-interactive
   alias vim="nvim"
+
   alias gco="git checkout"
   alias gst="git status -sb"
   alias gc="git commit"
@@ -16,6 +20,14 @@ if status is-interactive
   alias gpl="git pull"
   alias gl="git log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
   alias gps="git push"
-  alias cat="bat"
+
+  if type -q bat
+    alias cat="bat"
+  end
+
+  if type -q batcat
+    alias cat="batcat"
+  end
+
   alias print="lpr -o sides=two-sided-long-edge"
 end
