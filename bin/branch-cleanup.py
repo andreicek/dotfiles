32977ada792d7f8de7302b6a0397c8871f253f26
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2022-11-24 14:39:59 +0100

    Add bin folder

---
+++ bin/branch-cleanup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess
+from prompt_toolkit.shortcuts import checkboxlist_dialog
+
+cmd = 'git for-each-ref refs/heads --format="%(refname)"'
+status, output = subprocess.getstatusoutput(cmd)
+
+branches = []
+if status == 0:
+    for branch in output.split('\n'):
+      branch = branch.replace("refs/heads/", "")
+      branches.append((branch, branch))
+
+results_array = checkboxlist_dialog(
+    title="CheckboxList dialog",
+    text="What would you like in your breakfast ?",
+    values=branches
+).run()
+
+if results_array != None:
+  for branch in results_array:
+    cmd = f"git branch -D {branch}"
+    subprocess.getstatusoutput(cmd)
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -27,6 +27,3 @@ alias rm="trash"
 alias e="$EDITOR"
 
 source $ZSH/oh-my-zsh.sh
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
