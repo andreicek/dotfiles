ebf2645c42a01030b2ed6ea0c7fe0f8a4fd77428
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2024-01-11 22:32:04 +0100

    Add nvim setup

---
+++ nvim/.config/nvim/lua/plugins/elixir.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
+
+      elixir.setup({
+        nextls = { enable = true },
+        credo = { enable = false },
+        elixirls = { enable = false },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "elixir",
+        "heex",
+        "eex",
+      })
+    end,
+  },
+}
