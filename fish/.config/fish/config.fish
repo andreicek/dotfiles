cf79c59d71af5335ec59d30669f225c13fac3bb2
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2024-01-14 09:33:33 +0100

    Add lolcommits endpoint

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,52 +3,53 @@ source ~/.asdf/asdf.fish
 fish_add_path ~/.bin
 
 if status is-interactive
-  set -Ux EDITOR "vim"
-  set -Ux TERM "xterm"
-  set -Ux GPG_TTY (tty)
-  set -Ux ERL_AFLAGS "-kernel shell_history enabled"
+    set -Ux EDITOR vim
+    set -Ux TERM xterm
+    set -Ux LOLCOMMITS_DELAY 5
+    set -Ux GPG_TTY (tty)
+    set -Ux ERL_AFLAGS "-kernel shell_history enabled"
 
-  alias gco "git checkout"
-  alias gst "git status -sb"
-  alias gc "git commit"
-  alias gcm "git commit -m"
-  alias ga "git add"
-  alias gaa "git add -A"
-  alias gap "git add --patch"
-  alias gpl "git pull"
-  alias gps "git push"
-  alias gl "git log --oneline"
+    alias gco "git checkout"
+    alias gst "git status -sb"
+    alias gc "git commit"
+    alias gcm "git commit -m"
+    alias ga "git add"
+    alias gaa "git add -A"
+    alias gap "git add --patch"
+    alias gpl "git pull"
+    alias gps "git push"
+    alias gl "git log --oneline"
 
-  alias cat "bat"
-  alias rm "trash"
+    alias cat bat
+    alias rm trash
 
-  alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+    alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 end
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 function fish_prompt
-  if [ (_git_branch_name) ]
-    set git_info (_git_branch_name)
-    set git_info ":$git_info"
+    if [ (_git_branch_name) ]
+        set git_info (_git_branch_name)
+        set git_info ":$git_info"
 
-    if [ (_is_git_dirty) ]
-      set -l dirty "*"
-      set git_info "$git_info$dirty"
+        if [ (_is_git_dirty) ]
+            set -l dirty "*"
+            set git_info "$git_info$dirty"
+        end
     end
-  end
 
-  echo -n -s (basename (prompt_pwd)) $git_info "> "
+    echo -n -s (basename (prompt_pwd)) $git_info "> "
 end
 
 function fish_greeting
-  lolcat ~/.dotfiles/fish/.config/fish/motd.txt
+    lolcat ~/.dotfiles/fish/.config/fish/motd.txt
 end
 
 export PATH="$PATH:$HOME/.local/bin"
