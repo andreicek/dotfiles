796b1b3af3093457b1b428f5fbcac74819038677
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2024-06-07 21:43:36 +0200

    Neovim improvements

--- nvim/.config/nvim/lua/plugins/elixir.lua
+++ nvim/.config/nvim/lua/plugins/elixir.lua
@@ -1,30 +1,39 @@
 return {
-  {
-    "elixir-tools/elixir-tools.nvim",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
+	{
+		"elixir-tools/elixir-tools.nvim",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local elixir = require("elixir")
 
-      elixir.setup({
-        nextls = { enable = true },
-        credo = { enable = false },
-        elixirls = { enable = false },
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "elixir",
-        "heex",
-        "eex",
-      })
-    end,
-  },
+			elixir.setup({
+				nextls = {
+					enable = true,
+					spitfire = false,
+					init_options = {
+						experimental = {
+							completions = {
+								enable = true,
+							},
+						},
+					},
+				},
+				credo = { enable = false },
+				elixirls = { enable = false },
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"elixir",
+				"heex",
+				"eex",
+			})
+		end,
+	},
 }
