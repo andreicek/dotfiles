7534634db533857f3bca80ebe77ec0887ee69554
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2025-04-27 08:03:24 +0200

    Add notification on long running process finish in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -65,3 +65,14 @@ function fish_prompt
 
   echo -n -s (basename (prompt_pwd)) $git_info "> "
 end
+
+function notify_long_tasks --on-event fish_postexec
+    if test "$CMD_DURATION" -gt 5000
+        set -l cmd_name (string split ' ' -- $argv)[1]
+
+        if not contains $cmd_name ssh python psql node iex less cat
+            set -l duration_sec (math "$CMD_DURATION / 1000")
+            echo -e "\033]777;notify;Command finished in $duration_sec s;$argv\007"
+        end
+    end
+end
